5c082c747ce4fb78d83e43b88418401d5311e7ee
Alanah Jonas <156101954+alanahjonas95@users.noreply.github.com>
2026-01-16 09:50:26 +0000

    render qmd to create .r files

--- tlg/adverse_events.R
+++ tlg/adverse_events.R
@@ -57,3 +57,113 @@ lyt <- basic_table(show_colcounts = TRUE) %>%
 result <- build_table(lyt, df = adae, alt_counts_df = adsl)
 
 result
+
+## ----r message=FALSE, warning=FALSE-------------------------------------------
+library(pharmaverseadam) # for clinical trial data
+library(dplyr) # for data manipulation
+library(cards) # for creating analysis result displays
+library(tfrmt) # for formatting tables in R
+
+## ----r------------------------------------------------------------------------
+# Filter to include only subjects marked as part of the safety population
+adsl <- pharmaverseadam::adsl |>
+  filter(SAFFL == "Y")
+# Load adverse event data
+adae <- pharmaverseadam::adae |>
+  filter(SAFFL == "Y" & TRTEMFL == "Y")
+
+## ----r------------------------------------------------------------------------
+# Create an ARD that stacks hierarchical data of adverse events
+# Grouping by treatment, system organ class, and preferred term
+ae_ard <- ard_stack_hierarchical(
+  data = adae,
+  by = TRT01A, # Note: by variables must be present in the denominator dataset
+  variables = c(AEBODSYS, AETERM),
+  statistic = ~ c("n", "p"), # Calculate count and percentage
+  denominator = adsl,
+  id = USUBJID,
+  over_variables = TRUE,
+  overall = TRUE
+)
+
+# Filter adae and adsl with trt01a set to "Total" and create a new ARD for the total column
+adae2 <- adae |>
+  mutate(TRT01A = "Total")
+adsl2 <- adsl |>
+  mutate(TRT01A = "Total")
+
+ae2_ard <- ard_stack_hierarchical(
+  data = adae2,
+  by = TRT01A, # Note: by variables must be present in the denominator dataset
+  variables = c(AEBODSYS, AETERM),
+  denominator = adsl2,
+  statistic = ~ c("n", "p"),
+  id = USUBJID,
+  over_variables = TRUE,
+  overall = TRUE
+) |>
+  filter(group2 == "TRT01A" | variable == "TRT01A") # filter to stats we need
+
+## ----r------------------------------------------------------------------------
+ae3_ard <- bind_ard(ae_ard, ae2_ard) |>
+  # reshape the data
+  shuffle_card(fill_hierarchical_overall = "ANY EVENT") |>
+  # transform group-level freqs/pcts into a singular "bigN" row
+  prep_big_n(vars = "TRT01A") |>
+  # for nested variables, fill any missing values with "ANY EVENT"
+  prep_hierarchical_fill(vars = c("AEBODSYS", "AETERM"), fill = "ANY EVENT") |>
+  mutate(TRT01A = ifelse(TRT01A == "Overall TRT01A", "Total", TRT01A))
+
+# create ordering columns, sort by AEBODSYS
+ordering_aebodsys <- ae3_ard |>
+  filter(TRT01A == "Total", stat_name == "n", AETERM == "ANY EVENT") |>
+  arrange(desc(stat)) |>
+  mutate(ord1 = row_number()) |>
+  select(AEBODSYS, ord1)
+
+# sort by AETERM after AEBODSYS order
+ordering_aeterm <- ae3_ard |>
+  filter(TRT01A == "Total", stat_name == "n") |>
+  group_by(AEBODSYS) |>
+  arrange(desc(stat)) |>
+  mutate(ord2 = row_number()) |>
+  select(AEBODSYS, AETERM, ord2)
+
+# join on our ordering columns and keep required columns
+ae4_ard <- ae3_ard |>
+  full_join(ordering_aebodsys, by = "AEBODSYS") |>
+  full_join(ordering_aeterm, by = c("AEBODSYS", "AETERM")) |>
+  select(AEBODSYS, AETERM, ord1, ord2, stat, stat_name, TRT01A)
+
+## ----r------------------------------------------------------------------------
+AE_T01 <- tfrmt_n_pct(
+  n = "n", pct = "p",
+  pct_frmt_when = frmt_when(
+    "==1" ~ frmt("(100%)"),
+    ">=0.995" ~ frmt("(>99%)"),
+    "==0" ~ frmt(""),
+    "<=0.01" ~ frmt("(<1%)"),
+    "TRUE" ~ frmt("(xx.x%)", transform = ~ . * 100)
+  )
+) |>
+  tfrmt(
+    group = AEBODSYS,
+    label = AETERM,
+    param = stat_name,
+    value = stat,
+    column = TRT01A,
+    sorting_cols = c(ord1, ord2),
+    col_plan = col_plan(
+      "System Organ Class
+            Preferred Term" = AEBODSYS, Placebo, `Xanomeline High Dose`, `Xanomeline Low Dose`,
+      -ord1, -ord2
+    ),
+    row_grp_plan = row_grp_plan(row_grp_structure(
+      group_val = ".default", element_block(post_space = " ")
+    )),
+    big_n = big_n_structure(param_val = "bigN", n_frmt = frmt(" (N=xx)"))
+  ) |>
+  print_to_gt(ae4_ard)
+
+AE_T01
+
--- tlg/demographic.R
+++ tlg/demographic.R
@@ -81,3 +81,89 @@ lyt <- basic_table(show_colcounts = TRUE) |>
 result <- build_table(lyt, adsl2)
 
 result
+
+## ----r tfrmt-table------------------------------------------------------------
+library(cards)
+library(forcats)
+library(tfrmt)
+
+# build the ARD with the needed summary statistics using {cards}
+ard <-
+  ard_stack(
+    adsl,
+    ard_continuous(
+      variables = AGE,
+      statistic = ~ continuous_summary_fns(c("N", "mean", "sd", "min", "max"))
+    ),
+    ard_categorical(variables = c(AGEGR1, SEX, RACE)),
+    .by = ACTARM, # split results by treatment arm
+    .overall = TRUE,
+    .total_n = TRUE
+  )
+
+# tidy the ARD for use in {tfrmt}
+ard_tbl <-
+  ard |>
+  # reshape the data
+  shuffle_card(fill_overall = "Total") |>
+  # transform group-level freqs/pcts into a singular "bigN" row
+  prep_big_n(vars = "ACTARM") |>
+  # consolidate vars into a single variable column
+  prep_combine_vars(vars = c("AGE", "AGEGR1", "SEX", "RACE")) |>
+  # coalesce categorical levels + continuous stats into a "label"
+  prep_label() |>
+  group_by(ACTARM, stat_variable) |>
+  mutate(across(c(variable_level, label), ~ ifelse(stat_name == "N", "n", .x))) |>
+  ungroup() |>
+  unique() |>
+  # sorting
+  mutate(
+    ord1 = fct_inorder(stat_variable) |> fct_relevel("SEX", after = 0) |> as.numeric(),
+    ord2 = ifelse(label == "n", 1, 2)
+  ) |>
+  # relabel the variables
+  mutate(stat_variable = case_when(
+    stat_variable == "AGE" ~ "Age (YEARS) at First Dose",
+    stat_variable == "AGEGR1" ~ "Age Group (YEARS) at First Dose",
+    stat_variable == "SEX" ~ "Sex",
+    stat_variable == "RACE" ~ "High Level Race",
+    .default = stat_variable
+  )) |>
+  # drop variables not needed
+  select(ACTARM, stat_variable, label, stat_name, stat, ord1, ord2) |>
+  # remove duplicates (extra denominators per variable level)
+  unique()
+
+# create a demographics table using {tfrmt}
+DM_T01 <- tfrmt(
+  group = stat_variable,
+  label = label,
+  param = stat_name,
+  value = stat,
+  column = ACTARM,
+  sorting_cols = c(ord1, ord2),
+  body_plan = body_plan(
+    frmt_structure(group_val = ".default", label_val = ".default", frmt("xxx")),
+    frmt_structure(
+      group_val = ".default", label_val = ".default",
+      frmt_combine("{n} ({p}%)",
+        n = frmt("xxx"),
+        p = frmt("xx", transform = ~ . * 100)
+      )
+    )
+  ),
+  big_n = big_n_structure(param_val = "bigN", n_frmt = frmt(" (N=xx)")),
+  col_plan = col_plan(
+    -starts_with("ord")
+  ),
+  col_style_plan = col_style_plan(
+    col_style_structure(col = c("Placebo", "Xanomeline High Dose", "Xanomeline Low Dose", "Total"), align = "left")
+  ),
+  row_grp_plan = row_grp_plan(
+    row_grp_structure(group_val = ".default", element_block(post_space = " "))
+  )
+) |>
+  print_to_gt(ard_tbl)
+
+DM_T01
+
